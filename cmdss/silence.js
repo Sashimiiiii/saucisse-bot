6095d2a65fefa04cca531ee5cecfffa9e716534e
doriangat <doriangattesco@yahoo.fr>
2020-08-14 10:43:16 +0200

    Add files via upload

---
+++ cmdss/silence.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	name: 'silence',
+	description: 'tg moon',
+}
+
+module.exports.run = async (bot, message, args) => {
+
+    {
+{
+        message.delete();
+        var couteau = new Discord.Message()
+        message.channel.send("https://media.discordapp.net/attachments/730768784062939176/743582455230890074/90a_1__01.jpg");
+        
+        } 
+    }
+}
+
+module.exports.help = {
+    name: "silence"
+}
