4a9db1bc767fe316d372142a5af5363a8c901c81
doriangat <doriangattesco@yahoo.fr>
2020-08-24 14:22:53 +0200

    Update prefix.js

--- cmdss/prefix.js
+++ cmdss/prefix.js
@@ -15,10 +15,11 @@ module.exports.run = async (bot, message, args, prefix) => {
     prefixes[message.guild.id] = {
         prefixes: args[0]
     };
-
-	if (args[0] = undefined) {
+if (args[0] = undefined) {
             return message.channel.send(`Le prefix est vide!, ${message.author}!`);
-        }
+       
+	return; }
+	
 	
     fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
         if (err) console.log(err)
