610f02d6254f16000aa73dff4e6d14ff7c7b71f4
doriangat <doriangattesco@yahoo.fr>
2020-09-19 17:05:58 +0200

    Update join.js

--- cmdss/join.js
+++ cmdss/join.js
@@ -2,24 +2,28 @@ const Discord = require('discord.js');
 const fs = require('fs');
 var stream = require("ytdl-core");
 
-module.exports = {
-	name: 'join',
-	description: "Fait rejoindre le bot en vocal",
-}
-
 module.exports.run = async (bot, message, args) => {
   
 
     var voiceChannel = message.member.voiceChannel;
     message.delete();
-    voiceChannel.join().then(connection => {
-    console.log("joined channel");
-    const dispatcher = connection.playFile('./sons/bonjour.mp3');
-    dispatcher.setVolume(2.1);
+    var n=0;
     
-}).catch(err => console.log(err));
-isReady = true
-        }
+        voiceChannel.join().then(connection => {
+        console.log("joined channel");
+        const dispatcher = connection.playFile('./sons/caramel.mp3');
+        dispatcher.setVolume(2.1);
+        dispatcher.on("end", end => {
+
+            console.log("bot join");}
+        
+        );}
+        
+    ).catch(err => console.log(err));
+    isReady = true}
+        
+
+        
 module.exports.help = {
     name: "join"
 }
