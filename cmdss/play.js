a20fa36558ab46458c632e6d2acf7bdd892f543e
doriangat <doriangattesco@yahoo.fr>
2020-10-11 22:47:22 +0200

    Add files via upload

---
+++ cmdss/play.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+var ytdl = require("ytdl-core");
+
+module.exports = {
+	name: 'play [url]',
+	description: "Basiquement son youtube",
+}
+
+module.exports.run = async (bot, message, args) => {
+  
+
+    var channel = message.member.voiceChannel;
+    message.delete();
+    if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+        const dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=-JH5nhcxSyE', { filter: 'audioonly' }));
+
+      } else {
+        message.reply('Il faut etre dans le : channel vocal');
+    
+    
+    
+
+        }
+    }
+module.exports.help = {
+    name: "play"
+}
