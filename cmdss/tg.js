ea1a83590ebb4c41ca2d05da730511c4c7ad2621
doriangat <doriangattesco@yahoo.fr>
2020-10-14 01:01:45 +0200

    Update tg.js

--- cmdss/tg.js
+++ cmdss/tg.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
 {
         message.delete();
         var prefix = "s!tg" ;
-        const args = message.content.slice(prefix.length).trim().split(' ');
+        const args = message.content.slice(prefix.length).trim().split('/ +/');
         const command = args.shift().toUpperCase();
         var nom = command;
         message.channel.send("TA GUEULE "+ nom + " PUTAIN FERME TA GUEULE T4ES NUL T4ES QUUNE MERDE :mountain_cableway:ARRETE AVEC TES ANALYSES DE MERDE "+ nom + " TA VOIX 🤼‍♂INSSUPORTTABLE TA :necktie::necktie:GUEULE DE MERDE MONTRE LA PAS TU VAS RECEVOIR MES POINGS DEDANS JE VAIS T4EXPLOSER:office:PARLE ENCORE UNE FOIS JE TE DETRUIS "+ nom + " §§§§§")
