926461a03af89901351c92d2ef9b20e8ae7f1747
doriangat <doriangattesco@yahoo.fr>
2020-10-27 16:03:47 +0100

    Add files via upload

---
+++ oui/adieu.js
@@ -0,0 +1,50 @@
+
+module.exports = (bot) => {
+const Discord = require('discord.js');
+const fs = require('fs');
+const Canvas = require("canvas")
+const snekfetch = require("snekfetch")
+
+
+    bot.on('guildMemberRemove', async (member)=> {
+    
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'bloc-note')    //✋bienvenue
+    //channel.send('Salut <@'+ member.id +'>')
+
+    
+    console.log(member.displayName + ' a quitter le serveur')
+
+    
+    
+    const canvas = Canvas.createCanvas(2480, 3508);
+    const ctx = canvas.getContext("2d");
+    const background = await Canvas.loadImage("./images/saucisserevolutionultra.png")
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
+    
+    
+    //const {body:buffer} = await snekfetch.get(member.user.displayAvatarURL({ dynamic:true }));
+    //const avatar = await Canvas.loadImage(buffer);
+    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+    ctx.drawImage(avatar, 994, 2650, 527, 527);
+    
+    
+    ctx.font = "110px Futura";
+    ctx.fillStyle = "#ffffff";
+    ctx.textAlign = "center"
+    ctx.fillText(member.displayName + ' est parti : enculé de sauce' , 1240, 3400);
+    
+    
+    const attachment = new Discord.MessageAttachment(
+        canvas.toBuffer(),
+         'adieu.jpg'
+         );
+    
+    channel.send(attachment);
+    
+    
+    }
+
+
+
+)
+}
---
+++ oui/welcome.js
@@ -0,0 +1,55 @@
+
+module.exports = (bot) => {
+const Discord = require('discord.js');
+const fs = require('fs');
+const Canvas = require("canvas")
+const snekfetch = require("snekfetch")
+
+
+    bot.on('guildMemberAdd', async (member)=> {
+    
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'bloc-note')    //✋bienvenue
+    //channel.send('Salut <@'+ member.id +'>')
+
+    
+//setTimeout(() => {
+    console.log(member.displayName +' a rejoin le serveur')
+
+    var role = member.guild.roles.cache.find(r => r.name === "Saucissons");
+    member.roles.add(role).catch(console.error);
+    
+//}, 3000);
+    
+    
+    const canvas = Canvas.createCanvas(2480, 3508);
+    const ctx = canvas.getContext("2d");
+    const background = await Canvas.loadImage("./images/saucisserevolutionultra.png")
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
+    
+    
+    //const {body:buffer} = await snekfetch.get(member.user.displayAvatarURL({ dynamic:true }));
+    //const avatar = await Canvas.loadImage(buffer);
+    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+    ctx.drawImage(avatar, 994, 2650, 527, 527);
+    
+    
+    ctx.font = "220px Futura";
+    ctx.fillStyle = "#ffffff";
+    ctx.textAlign = "center"
+    ctx.fillText(member.displayName , 1240, 3400);
+    
+    
+    const attachment = new Discord.MessageAttachment(
+        canvas.toBuffer(),
+         'Welcome.jpg'
+         );
+    
+    channel.send(attachment);
+    
+    
+    }
+
+
+
+)
+}
