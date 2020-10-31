8e6805116a584bea6dd10d3eb66c4cc30265ab7e
doriangat <doriangattesco@yahoo.fr>
2020-11-01 00:24:03 +0100

    Update tts.js

--- cmdss/tts.js
+++ cmdss/tts.js
@@ -11,20 +11,21 @@ module.exports = {
 
 module.exports.run = async (bot, message,) => {
 
-    message.delete();
+    var user = message.author.tag
+
     if (message.member.voice.channel) {
         const connection = await message.member.voice.channel.join();
-    	var prefix = "s!tss" ;
+    var prefix = "s!tss" ;
         const args = message.content.slice(prefix.length).trim().split('/ +/');
         const command = args.shift().toUpperCase();
         var nom = command;
  
 googleTTS(nom, 'fr', 1)   // speed normal = 1 (default), slow = 0.24
 .then(function (url) {
-  
     const dispatcher = connection.play(url, {
         volume: 0.5,
     });
+    console.log("[TTS] " + user + " à tts: " + nom)
 })
 
 
@@ -32,7 +33,7 @@ googleTTS(nom, 'fr', 1)   // speed normal = 1 (default), slow = 0.24
 
 }else {
     message.reply('Il faut etre dans le : channel vocal');
-	}
+}
 }
 
 
