2ea4b892c8f0fcba08b34e3c42772446d26b9414
doriangat <doriangattesco@yahoo.fr>
2020-11-13 18:23:37 +0100

    Add files via upload

---
+++ cmdss/delire.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const Canvas = require("canvas")
+const snekfetch = require("snekfetch")
+
+module.exports = {
+	name: 'delire @nom',
+	description: 'Cible etre pas ton délire',
+}
+
+module.exports.run = async (bot, message, args, client) => {
+    
+    message.delete();
+
+    var member = message.guild.member(message.mentions.users.first());
+
+
+const canvas = Canvas.createCanvas(186, 84);
+const ctx = canvas.getContext("2d");
+const background = await Canvas.loadImage("./images/pasMonDelire.png")
+ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
+
+
+//const {body:buffer} = await snekfetch.get(member.user.displayAvatarURL({ dynamic:true }));
+//const avatar = await Canvas.loadImage(buffer);
+const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+ctx.drawImage(avatar, 104, 2, 80, 80);
+
+
+ //  ctx.font = "80px Arial";
+ //  ctx.fillStyle = "#ffffff";
+ //  ctx.fillText(member.displayName , 30, 210);
+
+
+const attachment = new Discord.MessageAttachment(
+    canvas.toBuffer(),
+     'delire.jpg'
+     );
+
+message.channel.send(attachment);
+
+
+
+var user = message.author.tag
+        var member = member.displayName
+        var today = new Date();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var date = today.getDate()+'/'+(today.getMonth()+1);
+        console.log("[DELIRE] " + user + " a utilisé DELIRE sur " + member + " | " + time + " Le " + date)
+
+
+}
+
+module.exports.help = {
+    name: "delire"
+}
