02f71833a268f80313af3a108695707f16433337
doriangat <doriangattesco@yahoo.fr>
2020-11-17 19:46:51 +0100

    Add files via upload

---
+++ cmdss/vocal/play.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+var ytdl = require("ytdl-core-discord");
+
+
+
+module.exports = {
+	name: 'play [URL]',
+	description: "Pour jouer lien YT",
+}
+
+
+module.exports.run = async (bot, message, args) => {
+  
+
+
+    message.delete();
+
+    
+    if (message.member.voice.channel) {
+        var prefix = "s!play" ;
+        const args = message.content.slice(prefix.length).trim().split('/ +/');
+        const url = args.shift().toString();
+        const connection = await message.member.voice.channel.join();
+        const dispatcher = connection.play(await ytdl(url), { 
+            type: 'opus',
+            volume: 0.1, 
+        
+        });
+      } else {
+        message.reply('Il faut etre dans le : channel vocal');
+        }
+    
+}
+        
+module.exports.help = {
+    name: "play"
+}
+
