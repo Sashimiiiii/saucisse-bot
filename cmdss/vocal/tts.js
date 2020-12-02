f1d9ed3b0340b92fc17c97e6085648e7200bed79
doriangat <doriangattesco@yahoo.fr>
2020-12-02 14:41:43 +0100

    Add files via upload

--- cmdss/vocal/tts.js
+++ cmdss/vocal/tts.js
@@ -1,4 +1,6 @@
-var googleTTS = require('google-tts-api');
+
+const fs = require('fs');
+const tts = require('google-translate-tts');
 
 module.exports = {
 	name: 'tts',
@@ -8,36 +10,34 @@ module.exports = {
 
 module.exports.run = async (bot, message,) => {
     message.delete();
-    var user = message.author.tag
 
-    if (message.member.voice.channel) {
-        const connection = await message.member.voice.channel.join();
     var prefix = "s!tts" ;
         const args = message.content.slice(prefix.length).trim().split('/ +/');
         const command = args.shift().toString();
         var nom = command;
- 
-googleTTS(nom, 'fr', 1)   // speed normal = 1 (default), slow = 0.24
-.then(function (url) {
-    const dispatcher = connection.play(url, {
+
+// notice that `tts.synthesize` returns a Promise<Buffer>
+const saveFile = async () => {
+    const buffer = await tts.synthesize({
+        text: nom,
+        voice: 'fr-FR'
+    });
+
+    fs.writeFileSync('hello-world.mp3', buffer);
+};
+
+saveFile();
+
+if (message.member.voice.channel) {
+    const connection = await message.member.voice.channel.join();
+    const dispatcher = connection.play('./hello-world.mp3', {
         volume: 0.5,
     });
-    var today = new Date();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var date = today.getDate()+'/'+(today.getMonth()+1);
-    console.log("[TTS] " + user + " a tts: " + nom + " | " + time + " Le " + date)
-})
-
-
-
-
 }else {
     message.reply('Il faut etre dans le : channel vocal');
 }
+
 }
-
-
-
         
 module.exports.help = {
     name: "tts"
--- cmdss/vocal/tts2.js
+++ cmdss/vocal/tts2.js
@@ -1,42 +1,41 @@
-var googleTTS = require('google-tts-api');
+
+const fs = require('fs');
+const tts = require('google-translate-tts');
 
 module.exports = {
-	name: 'tts2',
+	name: 'tts',
 	description: "pareil que au dessus mais ça met dans vocal among us",
 }
 
 
 module.exports.run = async (bot, message,) => {
     message.delete();
-    var user = message.author.tag
 
-        const connection = await bot.channels.cache.get("756241429923102751");
     var prefix = "s!tts2" ;
         const args = message.content.slice(prefix.length).trim().split('/ +/');
         const command = args.shift().toString();
         var nom = command;
- 
-googleTTS(nom, 'fr', 1)   // speed normal = 1 (default), slow = 0.24
-.then(function (url) {
-    connection.join().then(connection => {
-        const dispatcher = connection.play(url, {
-            volume: 0.5,
-        });
-    })
-    
-    var today = new Date();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var date = today.getDate()+'/'+(today.getMonth()+1);
-    console.log("[TTS2] " + user + " a tts: " + nom + " | " + time + " Le " + date)
-})
+
+// notice that `tts.synthesize` returns a Promise<Buffer>
+const saveFile = async () => {
+    const buffer = await tts.synthesize({
+        text: nom,
+        voice: 'fr-FR'
+    });
+
+    fs.writeFileSync('hello-world.mp3', buffer);
+};
+
+saveFile();
 
 
+    const connection = await bot.channels.cache.get("756241429923102751");
+    const dispatcher = connection.play('./hello-world.mp3', {
+        volume: 0.5,
+    });
 
 
 }
-
-
-
         
 module.exports.help = {
     name: "tts2"
