f8e1b667dc311bb92728725f4be8fa5480d6acc0
doriangat <doriangattesco@yahoo.fr>
2020-12-05 12:28:00 +0100

    Update monkeyDebut.js

--- oui/monkeyDebut.js
+++ oui/monkeyDebut.js
@@ -6,7 +6,7 @@ const d = new Date();
 
 
 console.log('Before job instantiation');
-const job = new CronJob('00 00 00 * * 6', function() {
+const job = new CronJob('00 00 01 * * 6', function() {
   console.log('At Ten Minutes:', d);
   
   
