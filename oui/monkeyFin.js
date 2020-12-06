445a46650fb65e8d42af0674e7e08ba7cbd08f38
doriangat <doriangattesco@yahoo.fr>
2020-12-06 01:49:07 +0100

    Update monkeyFin.js

--- oui/monkeyFin.js
+++ oui/monkeyFin.js
@@ -3,16 +3,16 @@ module.exports = (bot) => {
   var CronJob = require('cron').CronJob;
 const Discord = require('discord.js');
 const d = new Date();
-
+const channel = bot.channels.cache.get("730527992148656211");
 
 console.log('Before job instantiation');
-const job = new CronJob('00 00 01 * * 0', function() {
-  console.log('At Ten Minutes:', d);
+const job = new CronJob('00 50 01 * * 0', function() {
+  console.log('Fin du monkey samedi:', d);
   
   
   
  
-  const channel = bot.channels.cache.get("730527992148656211");
+  
   channel.send("Funky monkey samedi fini je : pleure")
 
 
