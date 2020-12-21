9c01b377e6596c744f7ec063b4cac6e2cf54b8c9
doriangat <doriangattesco@yahoo.fr>
2020-12-21 22:52:14 +0100

    Add files via upload

---
+++ cmdss/vocal/salut.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+var stream = require("ytdl-core");
+
+module.exports = {
+	name: 'salut',
+	description: "salut",
+}
+
+
+module.exports.run = async (bot, message, args) => {
+  
+
+    var voiceChannel = message.member.voiceChannel;
+    message.delete();
+    if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+        const dispatcher = connection.play('./sons/Salut.mp3', {
+            volume: 1,
+        });
+
+        //dispatcher.setVolume(0.1);
+        //dispatcher.on("end", end => {
+          //  console.log("left channel");
+
+      } else {
+        message.reply('Il faut etre dans le : channel vocal');
+    
+    }
+    
+    
+    
+
+        }
+module.exports.help = {
+    name: "salut"
+}
