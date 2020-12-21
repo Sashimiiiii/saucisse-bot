3ac87eccdebc978ce49c5eda4a92c94046835fb5
doriangat <doriangattesco@yahoo.fr>
2020-12-21 23:51:11 +0100

    Update salut.js

--- cmdss/vocal/salut.js
+++ cmdss/vocal/salut.js
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
     if (message.member.voice.channel) {
         const connection = await message.member.voice.channel.join();
         const dispatcher = connection.play('./sons/Salut.wav', {
-            volume: 0.5,
+            volume: 0.3,
         });
 
         //dispatcher.setVolume(0.1);
