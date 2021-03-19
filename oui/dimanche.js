ffdd23569e7e57b2be343443836bb2456d3e0a1b
doriangat <doriangattesco@yahoo.fr>
2021-03-19 15:10:47 +0100

    Add files via upload

---
+++ oui/dimanche.js
@@ -0,0 +1,25 @@
+module.exports = (bot) => {
+  var CronJob = require('cron').CronJob;
+const Discord = require('discord.js');
+const d = new Date();
+
+console.log('Before job instantiation');
+const job = new CronJob('00 00 23 * * 6', function() {
+
+  console.log('dimanche', d);
+  
+  
+  
+ 
+const channel = bot.channels.cache.get("730527992148656211");
+
+  channel.send({ files: ["./images/sunday.mp4"]});
+
+  
+      console.log("on est dimanche");
+
+});
+console.log('After job instantiation', d);
+job.start();
+
+}
