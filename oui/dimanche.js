fba9ec217bda8eef5342cd69bb4e014b3b703b23
doriangat <doriangattesco@yahoo.fr>
2021-04-02 01:02:17 +0200

    Update dimanche.js

--- oui/dimanche.js
+++ oui/dimanche.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const d = new Date();
 
 console.log('Before job instantiation');
-const job = new CronJob('00 00 23 * * 6', function() {
+const job = new CronJob('00 00 00 * * 0', function() {
 
   console.log('dimanche', d);
   
