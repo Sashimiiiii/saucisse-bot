45167b011a3f6f190e4aa622278c6dbe77511f1f
doriangat <doriangattesco@yahoo.fr>
2021-04-05 12:02:58 +0200

    Add files via upload

---
+++ oui/15h12.js
@@ -0,0 +1,25 @@
+module.exports = (bot) => {
+  var CronJob = require('cron').CronJob;
+const Discord = require('discord.js');
+const d = new Date();
+
+console.log('Before job instantiation');
+const job = new CronJob('00 12 13 * * *', function() {
+
+
+  
+  
+  
+ 
+const channel = bot.channels.cache.get("730527992148656211");
+
+  channel.send("<:Lydiens4:800410289442914365>");
+
+  
+
+
+});
+console.log('After job instantiation', d);
+job.start();
+
+}
