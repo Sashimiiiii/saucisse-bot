ee3ed532b4f240c56d89443a90972e0ce0bde23a
doriangat <doriangattesco@yahoo.fr>
2021-10-31 14:21:06 +0100

    Update lundi.js

--- oui/lundi.js
+++ oui/lundi.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const d = new Date();
 
 console.log('Before job instantiation');
-const job = new CronJob('00 00 22 * * 0', function() {
+const job = new CronJob('00 00 23 * * 0', function() {
 
 
   
