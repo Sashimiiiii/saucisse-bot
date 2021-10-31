a29144d2f1eed3aedde7df3411270a265ca27345
doriangat <doriangattesco@yahoo.fr>
2021-10-31 14:21:19 +0100

    Update mardi.js

--- oui/mardi.js
+++ oui/mardi.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const d = new Date();
 
 console.log('Before job instantiation');
-const job = new CronJob('00 00 22 * * 1', function() {
+const job = new CronJob('00 00 23 * * 1', function() {
 
 
   
