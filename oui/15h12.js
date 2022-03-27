962a9d5b212f8f81b6a470bf8515f38cfaa815d7
doriangat <doriangattesco@yahoo.fr>
2022-03-27 16:32:32 +0200

    Update 15h12.js

--- oui/15h12.js
+++ oui/15h12.js
@@ -2,22 +2,28 @@ module.exports = (bot) => {
   var CronJob = require('cron').CronJob;
 const Discord = require('discord.js');
 const d = new Date();
+const fs = require('fs');
 
 console.log('Before job instantiation');
-const job = new CronJob('00 12 13 * * *', function() {
+let hours = JSON.parse(fs.readFileSync("./hours.json", "utf8"));
 
+
+const baseHours = 14
+const BaseMinutes = 12
+
+const job = new CronJob(`00 ${BaseMinutes} ${baseHours + hours.hours} * * *`, function() {
 
   
   
   
- 
-const channel = bot.channels.cache.get("730527992148656211");
-
+  
+  const channel = bot.channels.cache.get("730527992148656211");
+  
   channel.send("<:Lydiens4:800410289442914365>");
-
   
-
-
+  
+  
+  
 });
 console.log('After job instantiation', d);
 job.start();
